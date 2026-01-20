8597ddb30ad154dc90ed78de0e3c921343c89e64
Jonathan Grimes <jsg2021@gmail.com>
2026-01-19 19:42:19 -0600

    map c-z to undo

--- configs/.config/nvim/lua/config/keymaps.lua
+++ configs/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+-- Prevent my muscle memory of Undo from backgrounding the editor :|
+-- vim.keymap.set({'n', 'i', 'v'}, '<C-z>', '<nop>', { silent = true })
+vim.keymap.set({ "n", "i", "v" }, "<C-z>", "u", { silent = true })
